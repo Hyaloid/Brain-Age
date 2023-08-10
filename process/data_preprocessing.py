39dd29e66ad8f18758cb16949f36a3b6648f1ea2
hyaloid <hyaloids@163.com>
2023-08-10 14:27:31 +0800

    异常数据删除

--- process/data_preprocessing.py
+++ process/data_preprocessing.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+
+color = sns.color_palette()
+sns.set_style('darkgrid')
 import matplotlib.pyplot as plt
 import warnings
 import os
+
+from scipy import stats
+from scipy.stats import norm, skew
 
 cur_dir = os.getcwd()
 print(cur_dir)
@@ -14,25 +21,39 @@ def ignore_warn(*args, **kwargs):
 
 warnings.warn = ignore_warn  # ignore warning
 
-from scipy import stats
-from scipy.stats import norm, skew
-
 pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x))  # 保留浮点数三位
-
 
 # 导入数据
 train = pd.read_csv('../data/input/data_train.csv')
 test = pd.read_csv('../data/input/data_test.csv')
+# train = pd.read_csv('../data/tmp_input/train.csv')
+# test = pd.read_csv('../data/tmp_input/test.csv')
 
+print([col for col in train])
 
 # 查看异常值
-fig, ax = plt.subplots()
-ax.scatter(x=train['Right-UnsegmentedWhiteMatter'], y=train['年龄'])
-plt.ylabel('', fontsize=13)
-plt.xlabel('Right-UnsegmentedWhiteMatter', fontsize=13)
+# for col in train:
+#     if col == "subject_ID":
+#         continue
+#     fig, ax = plt.subplots()
+#     ax.scatter(x=train[col], y=train['年龄'])
+#     plt.ylabel('年龄', fontsize=13)
+#     plt.xlabel(col, fontsize=13)
+#     plt.show()
 
+# print(train[(train['wm-rh-frontalpole'] > 700) & (train['年龄'] < 80)].index)
+
+sns.distplot(train['年龄'], fit=norm)
+
+(mu, sigma) = norm.fit(train['lh_GausCurv_caudalanteriorcingulate'])
+print('\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
+plt.ylabel('lh_GausCurv_caudalanteriorcingulate')
+plt.title('Age distribution')
+
+fig = plt.figure()
+res = stats.probplot(train['lh_GausCurv_caudalanteriorcingulate'], plot=plt)
 plt.show()
-
 
 # if __name__ == '__main__':
 #
